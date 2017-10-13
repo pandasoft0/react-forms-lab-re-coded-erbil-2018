9c0998904ed848391e9101691459aa95e7d6b02d
= <=>
2017-10-13 11:25:33 -0400

    updated react to 16, newest webpack, babel env and stage-1, new enzyme environment, switched root to global

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -1,43 +1,20 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
 class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {
-      message: ''
-    };
-  }
-
-  setMessage = event => {
-    this.setState({
-      message: event.target.value,
-    });
+    this.state = {};
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input 
-          type="text" 
-          value={this.state.message} 
-          onChange={this.setMessage} 
-        />
-        <span>{this.props.maxChars - this.state.message.length}</span>
+        <input type="text" />
       </div>
     );
   }
 }
-
-TwitterMessage.propTypes = {
-  maxChars: PropTypes.number,
-};
-
-TwitterMessage.defaultProps = {
-  maxChars: 140,
-};
-
 
 export default TwitterMessage;
