4c3d791827d69acb805e70f969bd6ca0caeefa1a
= <=>
2018-07-26 18:50:24 -0400

    updated tests, starter code, readme

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -1,41 +1,20 @@
 import React from "react";
-import PropTypes from "prop-types";
-//read more about proptypes here: https://reactjs.org/docs/typechecking-with-proptypes.html
 
 class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = { message: "" };
+    this.state = {};
   }
-
-  setMessage = event => {
-    this.setState({ message: event.target.value });
-  };
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input
-          type="text"
-          value={this.state.message}
-          onChange={this.setMessage}
-        />
-        <span>{this.props.maxChars - this.state.message.length}</span>
+        <input type="text" />
       </div>
     );
   }
 }
-
-// dont worry if you've never seen these. propTypes just gives better error messages
-// defaultProps sets a value to a prop if it isn't otherwise provided
-TwitterMessage.propTypes = {
-  maxChars: PropTypes.number,
-};
-
-TwitterMessage.defaultProps = {
-  maxChars: 140,
-};
 
 export default TwitterMessage;
